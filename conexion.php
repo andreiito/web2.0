e3639788e38fc1c85e187d98f841ecfbf59b7c48
andreiito <andreortiz11@hotmail.com>
2018-03-20 09:09:25 -0500

    ADD: Conexiòn e inserciòn de datos

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+//Permite establecer conexion al manejador de bases de datos:
+
+function consulta($consulta){
+	$con = pg_connect("host=127.0.0.1 port=5432 dbname=dbregistro user=registro_user password=holamundo") or die("No se puede establecer conexion a la BD");
+	$query = pg_query($con,$consulta);
+	$resp = pg_fetch_all($query);
+	return $resp;
+}
+?>
